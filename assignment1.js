378023c9b17c308153ddff3118a34a162c58655a
MUHAMMAD-TALHA-ATHAR <112604191+MUHAMMAD-TALHA-ATHAR@users.noreply.github.com>
2023-09-01 20:06:36 +0500

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,22 @@
+//Todo Assignment 1
+
+//! Question # 1
+
+// alert("Assalam o alaikum");
+
+//! Question # 2
+
+// alert("Error! Please enter a valid password.");
+
+//! Question # 3
+
+// alert("Welcome to JS Land...\nHappy coding!");
+
+//! Question # 4
+
+// alert("Welcome to JS Land...");
+// alert("Happy coding!\n▢ Prevent this page from additional dialogs.");
+
+//! Question # 5
+
+// console.log("Hello! I can run JS through web browser's console.")
---
+++ assignment2.js
@@ -0,0 +1,46 @@
+//Todo Assignment # 2
+
+
+//! Quetion # 1
+
+// var username;
+
+//! Question # 2
+
+// var myName;
+// myName = "Muhammad Talha Athar";
+
+//! Question # 3
+
+// var message;
+// message = "Hello world"
+// alert(message)
+
+//! Question # 4
+
+// var name = "Jhone Doe";
+// var age = "15 years old";
+// var job = "Certified Web Development"
+// alert(name);
+// alert(age);
+// alert(job);
+
+//! Question # 5
+
+// var word = "Pizza";
+// alert(word + "\n" +word.slice(0,4) + "\n" +word.slice(0,3) + "\n" +word.slice(0,2) + "\n" + word.slice(0,1));
+
+//! Question # 6
+
+// var email = "example@gmail.com";
+// alert("My email address is " + email)
+
+//! Questio # 7
+
+// var book = "A smarter way to learning JavaScript."
+// alert("I am trying to learn from the book " + book)
+
+//! Question # 9
+
+// var showString ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(showString)
